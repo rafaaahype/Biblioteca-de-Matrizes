bb9a8dd5397cfa20a082fb597e83aad38261646e
rafaelchep <rafaelsvieira777@gmail.com>
2026-03-31 02:26:05 -0300

    enxugando o mainc

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*
- * PROGRAMADORES: RAFAEL VIEIRA, JEFFERSON ALVES & TOMÁS FREITAS
+ * PROGRAMADORES: RAFAEL VIEIRA, JEFFERSON ALVES, TOMÁS FREITAS, RAY DAVYD
  */
 #include "bibliotecamatrizes.h"
 #include <stdio.h>
@@ -8,19 +8,22 @@ int main(){
 	printf("Digite a ordem de uma matriz (formato: linhaxcoluna): ");
 	int linha, coluna; scanf("%dx%d",&linha,&coluna);
 	int tamanho = linha*coluna+2;
-	float matrizA[tamanho];
+	float matrizA[tamanho], matrizB[tamanho], matrizC[tamanho];
 	matrizA[0] = linha; matrizA[1] = coluna;
+	matrizB[0] = linha; matrizB[1] = coluna;
+  	matrizC[0] = linha; matrizC[1] = coluna;
 
-	anularMatriz(matrizA);
-	exibirMatriz(matrizA);
-	adicionarElementoMatriz(matrizA, 1, 1, 2);
-	exibirMatriz(matrizA);
-	printf("\n\n%d",buscarElementoMatriz(matrizA, 1, 2));
+	preencherMatriz(matrizA);
+  	anularMatriz(matrizB);
+  	adicionarElementoMatriz(matrizB, 5, 1, 1);
+	somarMatrizes(matrizA, matrizB, matrizC);
+  	exibirMatriz(matrizC);
+
 	return 0;
 }
 
 /*
- * 
+ *
  * HÁ MAIS FUNÇÕES DISPONÍVEIS QUE O USUÁRIO PODE EXPLORAR À VONTADE.
  *
  */
