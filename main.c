0fc00e4d5a2d861da7b7fa37818fc9a19f3c4a23
rafaelchep <rafaelsvieira777@gmail.com>
2026-03-31 19:27:31 -0300

    ajustando main

--- main.c
+++ main.c
@@ -9,16 +9,48 @@ int main(){
 	int linha, coluna; scanf("%dx%d",&linha,&coluna);
 	int tamanho = linha*coluna+2;
 	float matrizA[tamanho], matrizB[tamanho], matrizC[tamanho];
-	matrizA[0] = linha; matrizA[1] = coluna;
-	matrizB[0] = linha; matrizB[1] = coluna;
-  	matrizC[0] = linha; matrizC[1] = coluna;
+	dimensionarMatriz(matrizA, linha, coluna);
+	printf("Digite a ordem de uma matriz (formato: linhaxcoluna): ");
+	scanf("%dx%d",&linha,&coluna);
+	tamanho = linha*coluna+2;
+	dimensionarMatriz(matrizB, linha, coluna);
+	dimensionarMatriz(matrizC, linha, coluna);
+
+	//main rafael
+	//float *matrizD = (float *)malloc(tamanho*sizeof(float));
+	//multiplicarMatrizRafael(matrizA, matrizB, &matrizD);
+	/*
+	preencherMatriz(matrizA);
+	anularMatriz(matrizB);
+	adicionarElementoMatriz(matrizB, 5, 1, 1);
+	somarMatrizes(matrizA, matrizB, matrizC);
+	exibirMatriz(matrizC);
+	*/
+/*
+	preencherMatriz(matrizA);
+	criarTransposta(matrizA, matrizB);
+	printf("\n\n\n");
+	exibirMatriz(matrizB);
+*/
+
+
+
 
 	preencherMatriz(matrizA);
-  	anularMatriz(matrizB);
-  	adicionarElementoMatriz(matrizB, 5, 1, 1);
-	somarMatrizes(matrizA, matrizB, matrizC);
-  	exibirMatriz(matrizC);
+	exibirMatriz(matrizA);
 
+	preencherMatriz(matrizB);
+	exibirMatriz(matrizB);
+
+	multiplicarMatriz(matrizA, matrizB, matrizC);
+
+	putchar('\n');
+
+	if(matrizC[0]==matrizA[0]&&matrizC[1]==matrizA[1])7
+		exibirMatriz(matrizC);
+
+		if(matrizIdentidade(matrizC)) printf("EH IDENTIDADE");
+		else printf("NAO EH IDENETIDADE");
 	return 0;
 }
 
